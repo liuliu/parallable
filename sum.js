c1766b0fc7d61e840fe2bc4a6af7eb715b4ebeef
Liu Liu <liuliu.1987@gmail.com>
2010-12-05 21:48:15 -0500

    added copyright

--- sum.js
+++ sum.js
@@ -1,5 +1,19 @@
+/*
+Copyright (c) 2010, Liu Liu
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the authors nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 if (parallable === undefined) {
 	var parallable = function (file, funct) {
+		/* if by any chance, JS engine embeded a hash implementation (MD5 or SHA1), I can get rid of the funct.toString() as key,
+		 * but for now, it is hard to copy several hundreds lines of code in the tiny snippet */
 		parallable.core[funct.toString()] = funct().core;
 		return function () {
 			var i;
@@ -11,6 +25,7 @@ if (parallable === undefined) {
 				for (i = 0; i < arguments.length - 2; i++)
 					params[i] = arguments[i];
 			} else {
+				/* support named argument */
 				async = arguments[0].async;
 				worker_num = arguments[0].worker;
 				params = arguments[0];
